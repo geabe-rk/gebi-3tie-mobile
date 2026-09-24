897ab2e7f537cfe0557250e528a61efb2fab4baf
gebi rovando <gebimobile06@gmail.com>
2026-09-24 01:29:15 -0800

    First Commit

---
+++ app/src/main/java/com/example/gebi_3tie_mobile/pertemuan3/ThirdActivity.kt
@@ -0,0 +1,35 @@
+package com.example.gebi_3tie_mobile.pertemuan3
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.gebi_3tie_mobile.R
+import com.example.gebi_3tie_mobile.databinding.ActivityThirdBinding
+import kotlin.jvm.java
+
+class ThirdActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThirdBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.btnKirim.setOnClickListener {
+            val intent = Intent(this, ThirdActivity::class.java)
+            startActivity(intent)
+
+            val noTujuan = binding.inputNoTujuan.text
+            Toast.makeText(this, "Pesan berhasil dikirim ke $noTujuan", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/gebi_3tie_mobile/pertemuan4/FourthActivity.kt
@@ -0,0 +1,21 @@
+package com.example.gebi_3tie_mobile.pertemuan4
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.gebi_3tie_mobile.R
+
+class FourthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_fourth)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
