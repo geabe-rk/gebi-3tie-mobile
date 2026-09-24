3830e22c17dec855e14233f63aa32ea9aa6412c7
gebi rovando <gebimobile06@gmail.com>
2026-09-24 01:46:41 -0800

    Second commit

--- app/src/main/java/com/example/gebi_3tie_mobile/MainActivity.kt
+++ app/src/main/java/com/example/gebi_3tie_mobile/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.gebi_3tie_mobile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.gebi_3tie_mobile.pertemuan4.FourthActivity
+import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val intent = Intent(this, FourthActivity::class.java)
+
+        /*tambahkan bagian berikut*/
+        intent.putExtra("name", "Politeknik Caltex Riau")
+        intent.putExtra("from", "Rumbai")
+        intent.putExtra("age", 25)
+
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/gebi_3tie_mobile/pertemuan3/ThirdActivity.kt
+++ app/src/main/java/com/example/gebi_3tie_mobile/pertemuan3/ThirdActivity.kt
@@ -2,34 +2,23 @@ package com.example.gebi_3tie_mobile.pertemuan3
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.gebi_3tie_mobile.R
-import com.example.gebi_3tie_mobile.databinding.ActivityThirdBinding
-import kotlin.jvm.java
+import com.example.gebi_3tie_mobile.pertemuan4.FourthActivity
 
 class ThirdActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityThirdBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        binding = ActivityThirdBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        binding.btnKirim.setOnClickListener {
-            val intent = Intent(this, ThirdActivity::class.java)
-            startActivity(intent)
 
-            val noTujuan = binding.inputNoTujuan.text
-            Toast.makeText(this, "Pesan berhasil dikirim ke $noTujuan", Toast.LENGTH_SHORT).show()
+        setContentView(R.layout.activity_third)
 
-        }
+        val intent = Intent(this, FourthActivity::class.java)
+
+        intent.putExtra("name", "Politeknik Caltex Riau")
+        intent.putExtra("age", 20)
+        intent.putExtra("from", "Rumbai")
+
+        startActivity(intent)
     }
 }
